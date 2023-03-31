59547bb0addb301fe22d315afcd882e2097a2805
crackbeka <77459105+crackbeka@users.noreply.github.com>
2023-03-31 18:31:11 +0400

    add video background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const video = document.getElementById('bgVideo') as HTMLVideoElement;
+    video.play();
+  }
+}
