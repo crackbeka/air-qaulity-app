f6575c0695ef3b4be60cb877cbe5e63ff2dffdc4
crackbeka <77459105+crackbeka@users.noreply.github.com>
2023-04-03 16:54:58 +0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,19 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   darkMode: boolean = true;
 
+  myData: any;
+
   ngOnInit(): void {
     const video = document.getElementById('bgVideo') as HTMLVideoElement;
     video.play();
 
-    const savedToggleState = localStorage.getItem('toggleState');
-    if (savedToggleState !== null) {
-      this.darkMode = JSON.parse(savedToggleState);
+    // Get the stored data from local storage
+    const storedData = localStorage.getItem('bgVideo');
+    if (storedData !== null) {
+      this.myData = JSON.parse(storedData);
     }
   }
 
   onToggleButtonClick() {
     this.darkMode = !this.darkMode;
     // Save the current state of the toggle button to local storage
-    localStorage.setItem('toggleState', JSON.stringify(this.darkMode));
+    localStorage.setItem('bgVideo', JSON.stringify(this.darkMode));
   }
 }
